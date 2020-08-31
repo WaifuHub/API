466864611774b553f01e53d0e0faad5e24e3a801
Ivan Nikitin <nikitin.ivan.dev@gmail.com>
2020-08-31 18:08:12 +0100

    Initial configs

---
+++ src/Application.kt
@@ -0,0 +1,19 @@
+package com.whub
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!", ContentType.Text.Plain)
+        }
+        get("/demo") {
+            call.respondText("HELLO WORLD!")
+        }
+    }
+}
